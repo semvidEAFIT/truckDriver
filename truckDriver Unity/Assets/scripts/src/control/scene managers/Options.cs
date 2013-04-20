27cd1023415dbd523d87c15444451628d8d82536
Hector Camilo Alzate Ramirez <halzate93@gmail.com>
2013-04-20 14:49:44 -0500

    Metas

--- truckDriver Unity/Assets/scripts/src/control/scene managers/Options.cs
+++ truckDriver Unity/Assets/scripts/src/control/scene managers/Options.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class Options : MonoBehaviour
 {
     public const string nodeCount = "nodeCount", time = "time", errorMargin = "errorMargin", dimensions = "dimensions";
-
+    private string currentNodeCount, currentTime, currentErrorMargin, currentDimension, x, y;
     private JSONObject difficultySettings;//time, dimensions, nodeCount, errorMargin
 
     private void Awake()
@@ -15,6 +15,13 @@ public class Options : MonoBehaviour
         }
 
         difficultySettings = JSONObject.Parse(PlayerPrefs.GetString("difficultySettings"));
+        JSONObject medium = difficultySettings.GetObject(Difficulty.Medium.ToString());
+        currentNodeCount = ((int)medium.GetNumber(nodeCount)).ToString();
+        currentTime = ((float)medium.GetNumber(time)).ToString();
+        currentErrorMargin = ((float)medium.GetNumber(errorMargin)).ToString();
+        JSONObject vector = medium.GetObject(dimensions);
+        x = ((int)vector.GetNumber("x")).ToString();
+        y = ((int)vector.GetNumber("y")).ToString();
     }
 
     public static void createNonExistentSettings()
@@ -35,7 +42,7 @@ public class Options : MonoBehaviour
         medium.Add(nodeCount, new JSONValue(10.0d));
         medium.Add(errorMargin, new JSONValue(0.5d));
         vector = new JSONObject();
-        vector.Add("x", new JSONValue(10.0d));
+        vector.Add("x", new JSONValue(7.0d));
         vector.Add("y", new JSONValue(10.0d));
         medium.Add(dimensions, vector);
         difficultySettings.Add(Difficulty.Medium.ToString(), medium);
@@ -56,6 +63,31 @@ public class Options : MonoBehaviour
 
     private void OnGUI()
     {
-        //edit difficulty settings
+        int groupWidth = Screen.width / 3, groupHeight = 3*Screen.height /5;
+        GUI.BeginGroup(new Rect(Screen.width/3, Screen.height/5, groupWidth, groupHeight));
+        GUI.Label(new Rect(0, (groupHeight / 5) * 0, groupWidth/2, groupHeight/5), nodeCount);
+        GUI.Label(new Rect(0, (groupHeight / 5) * 1, groupWidth / 2, groupHeight / 5), time);
+        GUI.Label(new Rect(0, (groupHeight / 5) * 2, groupWidth/2, groupHeight/5), errorMargin);
+        GUI.Label(new Rect(0, (groupHeight / 5) * 3, groupWidth / 2, groupHeight / 5), dimensions);
+        currentNodeCount = GUI.TextField(new Rect(groupWidth/2, (groupHeight / 5) * 0, groupWidth / 2, groupHeight / 5), currentNodeCount);
+        currentTime = GUI.TextField(new Rect(groupWidth / 2, (groupHeight / 5) * 1, groupWidth / 2, groupHeight / 5), currentTime);
+        currentErrorMargin = GUI.TextField(new Rect(groupWidth / 2, (groupHeight / 5) * 2, groupWidth / 2, groupHeight / 5), currentErrorMargin);
+        x = GUI.TextField(new Rect(groupWidth / 2, (groupHeight / 5) * 3, groupWidth / 4, groupHeight / 5), x);
+        y = GUI.TextField(new Rect(groupWidth / 2 + groupWidth / 4, (groupHeight / 5) * 3, groupWidth / 4, groupHeight / 5), y);
+        if(GUI.Button(new Rect(0, (groupHeight / 5) * 4,groupWidth,groupHeight/5),"Save")){
+            JSONObject medium = new JSONObject();
+            difficultySettings.Remove(Difficulty.Medium.ToString());
+            medium.Add(nodeCount, new JSONValue(int.Parse(currentNodeCount)));
+            medium.Add(time, new JSONValue(float.Parse(currentTime)));
+            medium.Add(errorMargin, new JSONValue(float.Parse(currentErrorMargin)));
+            JSONObject vector = new JSONObject();
+            vector.Add("x", new JSONValue(int.Parse(x)));
+            vector.Add("y", new JSONValue(int.Parse(y)));
+            medium.Add("dimensions", vector);
+            difficultySettings.Add(Difficulty.Medium.ToString(), medium);
+            PlayerPrefs.SetString("difficultySettings", difficultySettings.ToString());
+            PlayerPrefs.Save();
+        }
+        GUI.EndGroup();
     }
 }
