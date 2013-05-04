b6686eca0f913456b4dce7bc3048dfa0d4871dc2
Juan Moreno <moreno.129@hotmail.com>
2013-05-04 14:53:09 -0500

    Tutorial

---
+++ truckDriver Unity/Assets/scripts/src/scene managers/Tutorial.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class Tutorial : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	private void OnGUI(){
+		GUI.Box(new Rect(10,10,100,90), "");
+		//if(GUI.Button ((new Rect (Screen.width - 100,Screen.height - 50,100,50), "Bottom-right"))	
+		//{
+
+		//}
+	}
+}
