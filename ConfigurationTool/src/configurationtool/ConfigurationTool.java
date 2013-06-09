e90db56147ad24085b23f0c189debea1b250b2b8
Héctor Alzate <halzate93@gmail.com>
2013-06-09 16:56:37 -0500

    Configuration Tool general setup

---
+++ ConfigurationTool/src/configurationtool/ConfigurationTool.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package configurationtool;
+
+/**
+ *
+ * @author halzate93
+ */
+public class ConfigurationTool {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
